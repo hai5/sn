6f06a6465ec5cfb49cfbd577ef10df24fb0831a5
Cong Hai Nguyen <nconghai02@gmail.com>
2016-09-04 10:11:43 +0700

    fix

---
+++ quitErr.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#author: Nguyen Cong Hai
+#created: 2016-07-05
+
+cat << EOF
+quitErr () {
+ # perform some steps prior to quit with error:
+ # echo "\$0: \$*. Filetypes so far: \${gbSnipArr}" >&2
+ echo "sn: \$*" >&2 
+ shift 1
+ if [ \$# -gt 0 ] ; then
+     exit "\$1"
+ else
+  exit 1
+ fi
+} # quitErr
+EOF
